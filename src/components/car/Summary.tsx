8f46262b6bf2c40031539d5ca8a58bfaf5e7ad94
raggi.izar <raggi.izar@aoatechpartners.com.br>
2023-07-05 17:34:14 -0300

    feat: summary layout part done

--- src/components/car/Summary.tsx
+++ src/components/car/Summary.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import PrimaryColor from "../../assets/icons/primary-color.svg"
 
-export const Summary = ({handleUpdateEnviroment}: any) => {
+export const Summary = ({handleUpdateEnviroment}: any) => { // TODO: fix types
   const [internal, setInternal] = useState(false);
 
   function updateInternal(data: boolean) {
@@ -10,11 +11,35 @@ export const Summary = ({handleUpdateEnviroment}: any) => {
 
   return (
     <div className="flex flex-col w-[20vw] bg-[#1C1C1C] items-center py-8 px-12 text-white">
-      <div className="flex flex-col">
+      <div className="flex flex-col w-[214px]">
         <strong className="font-inter font-semibold uppercase">view</strong>
         <div className="flex font-inter uppercase font-medium text-xs mt-4 cursor-pointer">
           <div className={`rounded-s-[10px] border border-[#E2B558] ${internal && 'bg-[#E2B558]'} py-[6px] px-6`} onClick={() => updateInternal(true)}>interior</div>
           <div className={`rounded-e-[10px] border border-[#E2B558] ${!internal && 'bg-[#E2B558]'} py-[6px] px-6`} onClick={() => updateInternal(false)}>exterior</div>
+        </div>
+      </div>
+      <div className="flex flex-col w-[214px] mt-14">
+        <strong className="font-inter font-semibold uppercase">summary</strong>
+        <div className="flex flex-row mt-8">
+          <div className="flex flex-col items-center py-1">
+            <div className="h-3 w-3 bg-[#7d7d7d] rounded" />
+            <div className="w-px bg-[#7d7d7d] h-96 mt-4"></div>
+          </div>
+          <div className="flex flex-col flex-1">
+            <span className="font-inter text-sm ml-4">Exterior design</span>
+            <div className="flex justify-between items-center my-8">
+              <div className="flex flex-row items-center relative">
+                <div className="icon-holder bg-[#1C1C1C] p-1 absolute left-[-18px]">
+                  <PrimaryColor />
+                </div>
+                <div className="flex flex-col pl-4">
+                  <small className="font-space text-[10px]">Primary color</small>
+                  <span className="text-[#E2B558] font-space text-xs">$ 00.0</span>
+                </div>
+              </div>
+              <div className="w-4 h-8 redGradient"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/car/scene/Porsche.tsx
+++ src/components/car/scene/Porsche.tsx
@@ -122,7 +122,7 @@ export default function Model({exteriorDesign, interiorDesign}: CarConfigs) {
   const { nodes, materials } = useGLTF('/porsche/scene.gltf') as GLTFResult
 
   const primaryColorMaterial = new THREE.MeshPhysicalMaterial({ color: exteriorDesign.primaryColor.value, roughness: 0.2, metalness: exteriorDesign.primaryColor.value === '#ffee05' || exteriorDesign.primaryColor.value === '#1a6dd9' ? 0.2 : 0.8 });
-  const secondaryColorMaterial = new THREE.MeshPhysicalMaterial({ color: exteriorDesign.secondaryColor.value === 'carbon' ? 'white' : exteriorDesign.secondaryColor.value, roughness: 0.2, metalness: exteriorDesign.secondaryColor.value === '#ffee05' || exteriorDesign.secondaryColor.value === '#1a6dd9' ? 0.2 : 0.8 });
+  const secondaryColorMaterial = new THREE.MeshPhysicalMaterial({ color: exteriorDesign.secondaryColor.value, roughness: 0.2, metalness: exteriorDesign.secondaryColor.value === '#ffee05' || exteriorDesign.secondaryColor.value === '#1a6dd9' ? 0.2 : 0.8 });
   const wheelMaterial = new THREE.MeshStandardMaterial({ color: 'silver', roughness: 0.2, metalness: 0.8, side: THREE.DoubleSide  });
   const braksMaterial = new THREE.MeshStandardMaterial({ color: exteriorDesign.brakesColor.value, roughness: 0.2, metalness: 0.5, side: THREE.DoubleSide });
 
