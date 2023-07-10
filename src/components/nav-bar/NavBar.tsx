17a01f6133978cbdd4655276c659a0e6961e1d87
raggi.izar <raggi.izar@aoatechpartners.com.br>
2023-07-10 14:54:44 -0300

    feat: sidebar

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../../assets/icons/menu.svg";
 import Profile from "../../assets/icons/profile.svg";
 import Logo from "../../assets/porscha-logo-white.png";
@@ -6,9 +6,16 @@ import { Link } from "react-router-dom";
 import { SideBar } from "./SideBar";
 
 export const NavBar = () => {
+
+  const [sideBarOpen, setSideBarOpen] = useState(false)
+
+  const closeSideBar = () => {
+    setSideBarOpen(false)
+  }
+
   return (
     <div className="flex justify-between items-center w-full py-4 px-10 bg-[#161616] relative z-20">
-      <div className="cursor-pointer text-white">
+      <div className="cursor-pointer text-white" onClick={() => setSideBarOpen(true)}>
         <Menu />
       </div>
       <Link to={"/"}>
@@ -17,7 +24,7 @@ export const NavBar = () => {
       <div className="cursor-pointer text-white">
         <Profile />
       </div>
-      {/* <SideBar /> */}
+      <SideBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen}/>
     </div>
   );
 };
--- src/components/nav-bar/SideBar.tsx
+++ src/components/nav-bar/SideBar.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { AnimatePresence, motion } from "framer-motion";
+import p911 from "../../assets/car-models/911.webp";
+import p718 from "../../assets/car-models/718.webp";
+import cayenne from "../../assets/car-models/cayenne.webp";
+import macan from "../../assets/car-models/macan.webp";
+import panamera from "../../assets/car-models/panamera.webp";
+import React from "react";
 
-export const SideBar = () => {
+export const SideBar = ({ sideBarOpen, setSideBarOpen }: any) => {
   return (
-    <div className='w-[35vw] h-screen bg-[#1C1C1C] absolute left-0 top-0 bottom-0 shadow-xl'>SideBar</div>
-  )
-}
+    <AnimatePresence>
+      {sideBarOpen && (
+        <>
+          <motion.div
+            className="h-screen bg-[#1C1C1C] absolute left-0 top-0 bottom-0 shadow-xl z-10 flex flex-col p-12"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: "25vw", opacity: 1 }}
+            exit={{ width: 0, opacity: 0 }}
+            transition={{type: "spring",stiffness: 250,damping: 24}}
+          >
+            <span className="text-white font-montserrat uppercase font-medium">models</span>
+            <div className="mt-8">
+              <div className="flex justify-center items-center rounded-[10px] p-4 w-full hover:bg-neutral-800 cursor-pointer transition ease-in-out mb-4">
+                <img src={p911} alt="" width={'200px'} />
+              </div>
+              <div className="flex justify-center items-center rounded-[10px] p-4 w-full hover:bg-neutral-800 cursor-pointer transition ease-in-out mb-4">
+                <img src={p718} alt="" width={'200px'} />
+              </div>
+              <div className="flex justify-center items-center rounded-[10px] p-4 w-full hover:bg-neutral-800 cursor-pointer transition ease-in-out mb-4">
+                <img src={cayenne} alt="" width={'200px'} />
+              </div>
+              <div className="flex justify-center items-center rounded-[10px] p-4 w-full hover:bg-neutral-800 cursor-pointer transition ease-in-out mb-4">
+                <img src={macan} alt="" width={'200px'} />
+              </div>
+              <div className="flex justify-center items-center rounded-[10px] p-4 w-full hover:bg-neutral-800 cursor-pointer transition ease-in-out mb-4">
+                <img src={panamera} alt="" width={'200px'} />
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute left-0 top-0 bottom-0 h-screen w-screen bg-black"
+            animate={{ opacity: 0.5 }}
+            initial={{ opacity: 0 }}
+            exit={{opacity: 0}}
+            onClick={() => setSideBarOpen(false)}
+          />
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
