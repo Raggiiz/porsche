67694f1e5273bc00addcfd1d08aeec468e29bbe6
raggi.izar <raggi.izar@aoatechpartners.com.br>
2023-08-13 20:31:53 -0300

    feat(home): motion effects

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -14,27 +14,44 @@ export const Home = () => {
       transition={{ duration: 1 }}
     >
       <div
-        className="main-content absolute left-0 right-0 top-0 bottom-0 h-screen w-screen 
+        className="main-content absolute left-0 right-0 top-0 bottom-0 h-screen w-screen overflow-hidden
         object-cover z-10 flex flex-col justify-center bg-gradient-to-r from-[#00000066] to-50%"
       >
         <div className="px-40 pt-72 flex flex-col h-full justify-between">
           <div className="flex flex-row">
             <div className="flex flex-col w-1/2">
-              <h1 className="font-porsche text-7xl text-white">911 gt2</h1>
-              <p className="text-white w-[500px] font-montserrat text-sm font-medium mt-3">
+              <motion.h1 className="font-porsche text-7xl text-white"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1}}>
+                911 gt2
+              </motion.h1>
+              <motion.p className="text-white w-[500px] font-montserrat text-sm font-medium mt-3"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1.2}}>
                 A masterpiece of automotive engineering. Combining unrestrained
                 power, iconic design, and advanced technology, it embodies the
                 passion. Every curve, every acceleration is a unique and
                 unforgettable experience.
-              </p>
-              <Link to={"/911/gt2/configurator"} className="primary-btn mt-6">
-                Customize vehicle
-                <div className="arrow">
-                  <ArrowRight />
-                </div>
-              </Link>
+              </motion.p>
+              <motion.div 
+              className="mt-6"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1.3}}>
+                <Link to={"/911/gt2/configurator"} className="primary-btn">
+                  Customize vehicle
+                  <div className="arrow">
+                    <ArrowRight />
+                  </div>
+                </Link>
+              </motion.div>
             </div>
-            <div className="flex w-1/2 justify-center">
+            <motion.div className="flex w-1/2 justify-center"
+            animate={{opacity: 1}}
+            initial={{opacity: 0}}
+            transition={{delay: 1.4}}>
               <div className="bg-[#0000004D] backdrop-blur-md rounded-[10px] p-5 text-white">
                 <strong className="font-space uppercase text-base">
                   meet the configurator
@@ -47,10 +64,13 @@ export const Home = () => {
                   Get the summary with the total car price to send it to your dealer.
                 </p>
               </div>
-            </div>
+            </motion.div>
           </div>
           <div className="flex flex-row items-end">
-            <div className="all-slider relative mb-28">
+            <motion.div className="all-slider relative mb-28"
+            animate={{opacity: 1}}
+            initial={{opacity: 0}}
+            transition={{delay: 1.5}}>
               <div className="clip-path bg-[#0000004D] backdrop-blur-md rounded-[10px] p-5 text-white">
                 <strong className="font-space uppercase text-base">
                   sport car of the
@@ -73,31 +93,43 @@ export const Home = () => {
                   <ArrowRight />
                 </div>
               </div>
-            </div>
+            </motion.div>
             <div className="flex flex-row justify-between backdrop-blur-md bg-[#0000004D] h-52 w-3/5 rounded-ss-[10px] absolute right-0 pr-40 pl-32 py-12 font-inter">
-              <div className="flex flex-col text-white">
+              <motion.div className="flex flex-col text-white"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1.6}}>
                 <span className="text-base font-medium">Aceleration</span>
                 <small className="text-sm text-[#B7B7B7] font-light">0 to 100km/h</small>
                 <strong className="font-space text-xl">3.5 seg</strong>
-              </div>
+              </motion.div>
               <div className="h-2/3 w-px bg-white"></div>
-              <div className="flex flex-col text-white">
+              <motion.div className="flex flex-col text-white"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1.7}}>
                 <span className="text-base font-medium">Horse Power</span>
                 <small className="text-sm text-[#B7B7B7] font-light">6 shifts</small>
                 <strong className="font-space text-xl">560</strong>
-              </div>
+              </motion.div>
               <div className="h-2/3 w-px bg-white"></div>
-              <div className="flex flex-col text-white">
+              <motion.div className="flex flex-col text-white"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1.8}}>
                 <span className="text-base font-medium">Maximum speed</span>
                 <small className="text-sm text-[#B7B7B7] font-light">650 rpm</small>
                 <strong className="font-space text-xl">390 km/h</strong>
-              </div>
+              </motion.div>
               <div className="h-2/3 w-px bg-white"></div>
-              <div className="flex flex-col text-white">
+              <motion.div className="flex flex-col text-white"
+              animate={{y: 0, opacity: 1}}
+              initial={{y: 75, opacity: 0}}
+              transition={{delay: 1.9}}>
                 <span className="text-base font-medium">Tork</span>
                 <small className="text-sm text-[#B7B7B7] font-light">1750-450 rpm</small>
                 <strong className="font-space text-xl">479 lb-ft</strong>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
